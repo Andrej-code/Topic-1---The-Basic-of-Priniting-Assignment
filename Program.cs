2eb4ae7574340cb194259544c850d2955c14a03e
Andrej Trajkovic <700007609@GoTVDSB.ca>
2025-02-04 14:45:15 -0500

    Finished

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Topic_1___The_Basic_of_Priniting_Assignment
+﻿using System;
+
+namespace Topic_1___The_Basic_of_Priniting_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Andrej Trajkovic
+            Console.WriteLine("Hello, My Name is Andrej Trajkovic");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used for when your code is very complicated so it would be more easier for people to read & it could be used for letting people know how your code will work. ");
+            Console.WriteLine(); // You could make a space for a senctence by just put nothing in the ()'s.
+            Console.WriteLine("  A    TTTTTTT");
+            Console.WriteLine(" A A      T");
+            Console.WriteLine("A   A     T");
+            Console.WriteLine("AAAAA     T");
+            Console.WriteLine("A   A     T");
+            Console.WriteLine("A   A     T");
+            Console.WriteLine("A   A     T");
         }
+
     }
 }
