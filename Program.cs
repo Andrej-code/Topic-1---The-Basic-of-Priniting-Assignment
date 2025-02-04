d7a44d5fa1cbdadbbef7cc2e8b71feb988ae3716
Andrej Trajkovic <700007609@GoTVDSB.ca>
2025-02-04 14:17:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_1___The_Basic_of_Priniting_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
